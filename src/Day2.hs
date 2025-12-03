3ff3268be8825530d5efb2cc2cb083dcb7114bd3
philip <philipsandmann@gmail.com>
2025-12-04 00:08:25 +0100

    day2 part2

--- src/Day2.hs
+++ src/Day2.hs
@@ -8,8 +8,17 @@ day2 = do
     input <- readFile "/home/philip/git/aoc2025/inputs/day2.txt"
     let part1 = solve1 input
     print part1
+    let part2 = solve2 input  
+    print part2
 
 solve1 :: String -> Int  
 solve1 =  sum . map read . filter (\s -> s =~ pattern ) . concatMap (\s -> rangeToList s) . splitOn ","
     where rangeToList  = map show . (\([start,end]) -> [start..end]) . map (\s -> read s :: Int) . splitOn "-"
           pattern = "^(\\d+)\\1$"
+
+solve2 :: String -> Int  
+solve2 =  
+    sum . map read . 
+    filter (\s -> s =~ pattern ) . concatMap (\s -> rangeToList s) . splitOn ","
+    where rangeToList  = map show . (\([start,end]) -> [start..end]) . map (\s -> read s :: Int) . splitOn "-"
+          pattern = "^(\\d+)\\1{1,}$"
