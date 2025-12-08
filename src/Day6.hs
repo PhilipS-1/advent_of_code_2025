c58da7613b139a40ac1793f4d523eb366cf5aa00
philip <philipsandmann@gmail.com>
2025-12-09 00:24:11 +0100

    day6 part2

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,7 +2,9 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 module Day6 (day6, parseOp) where
 
+import Data.List.Split (splitWhen)
 import Data.List (transpose)
+import Data.String.Utils (strip)
 
 day6 :: IO ()
 day6 = do 
@@ -10,6 +12,9 @@ day6 = do
     let equations = parseInput input 
     let part1 = solve1 equations
     print part1 
+    let equations2 = parseInput2 input
+    let part2 = solve2 equations2
+    print part2 
 
 parseInput :: String -> [[String]]
 parseInput = transpose . map words . lines
@@ -25,6 +30,22 @@ calcEq eq acc = acc + foldr op start eqNums
             | last eq == "+" = 0
             | otherwise = 1 
 
-parseOp :: Num a => String -> (a -> a -> a)
+
+parseOp :: Num a => String -> a -> a -> a
 parseOp "+" = (+)
 parseOp "*" = (*)
+
+parseInput2 :: String -> [[String]]
+parseInput2 = splitWhen (== "") . map strip . transpose . lines 
+-- output = [["1  *","24","356"],["369+","248","8"],["32*","581","175"],["623+","431","4"]]
+
+solve2 :: [[String]] -> Int 
+solve2 = foldr calcEq2 0 
+
+calcEq2 :: [String] -> Int -> Int 
+calcEq2 eq acc = acc + foldr op start eqNums
+    where op = parseOp $ (: []) $ last $ head eq 
+          eqNums = map (read . takeWhile (\c -> c /= '*' && c /= '+')) eq 
+          start
+            | (last $ head eq) == '+' = 0
+            | otherwise = 1  
